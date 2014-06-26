e7e8a1282f7f0c9019e9e9feb1ce4168a114c19b
Ryan French <ryan.french@mubaloo.com>
2014-06-26 10:02:14 +0100

    test page rendering, but not working

--- brocfile.js
+++ brocfile.js
@@ -1,12 +1,22 @@
-
 var filterTemplates = require('broccoli-template')
-//var uglifyJavaScript = require('broccoli-uglify-js')
 var compileES6 = require('broccoli-es6-concatenator')
 var compileSass = require('broccoli-compass')
 var pickFiles = require('broccoli-static-compiler')
 var mergeTrees = require('broccoli-merge-trees')
-var findBowerTrees = require('broccoli-bower')
+//var findBowerTrees = require('broccoli-bower')
 var env = require('broccoli-env').getEnv()
+
+var appFilesToAppend = [
+    'jQuery/dist/jquery.min.js',
+    'handlebars/handlebars.min.js',
+    'ember/ember.js',
+    'ember-resolver/dist/ember-resolver.js'
+  ]
+
+var testFilesToAppend = [
+  'qunit/qunit/qunit.js',
+  'ember-qunit/dist/named-amd/main.js'
+];
 
 //change this for each project!
 var appNamespace = 'broccoli-ember';
@@ -20,6 +30,8 @@ function preprocess (tree) {
   return tree
 }
 
+var bowerComps = 'bower_components';
+
 var app = 'app'
 app = pickFiles(app, {
   srcDir: '/',
@@ -32,41 +44,60 @@ styles = pickFiles(styles, {
   srcDir: '/',
   destDir: appNamespace + '/styles'
 })
-styles = preprocess(styles)
 
-var tests = 'tests'
-tests = pickFiles(tests, {
+var testFiles = 'tests'
+testFiles = pickFiles(testFiles, {
   srcDir: '/',
-  destDir: appNamespace + '/tests'
+  files:['index.html'],
+  destDir: '/tests'
 })
-tests = preprocess(tests)
+
+var testsScripts = 'tests'
+testsScripts = pickFiles(testsScripts, {
+  srcDir: '/',
+  files:['**/*.js'],
+  destDir: '/tests'
+})
 
 var vendor = 'vendor'
+var sourceTrees = [app, styles, vendor, bowerComps]
 
-var sourceTrees = [app, styles, vendor]
-if (env !== 'production') {
-  sourceTrees.push(tests)
-}
-sourceTrees = sourceTrees.concat(findBowerTrees())
+//sourceTrees = sourceTrees.concat(findBowerTrees())
 
 var appAndDependencies = new mergeTrees(sourceTrees, { overwrite: true })
 
 var appJs = compileES6(appAndDependencies, {
   loaderFile: 'loader.js',
   ignoredModules: [
-    'ember/resolver'
+    'ember/resolver',
+    'qunit',
+    'ember-qunit'
   ],
   inputFiles: [
     appNamespace + '/**/*.js'
   ],
-  legacyFilesToAppend: [
-    'jquery.js',
-    'handlebars.js',
-    'ember.js',
-    'ember-resolver.js'
-  ],
+  legacyFilesToAppend: appFilesToAppend,
   wrapInEval: false,
   outputFile: '/assets/app.js'
+})
+
+// TESTING
+var testSourceTrees = [app, testsScripts, vendor, bowerComps];
+//testSourceTrees = testSourceTrees.concat(findBowerTrees())
+var testAndDependencies = new mergeTrees(testSourceTrees, { overwrite: true })
+var testsJs = compileES6(testAndDependencies, {
+  loaderFile: 'loader.js',
+  ignoredModules: [
+    'ember/resolver',
+    'qunit',
+    'ember-qunit'
+  ],
+  inputFiles: [
+    'tests/**/*.js'
+  ],
+  legacyFilesToAppend: appFilesToAppend.concat(testFilesToAppend),
+  wrapInEval: false,
+  outputFile: '/assets/tests.js'
 })
 
 var appCss = compileSass(appAndDependencies, appNamespace + '/styles/app.scss', {
@@ -78,4 +109,4 @@ var appCss = compileSass(appAndDependencies, appNamespace + '/styles/app.scss', 
 
 var publicFiles = 'public'
 
-module.exports = mergeTrees([appJs, appCss, publicFiles])
+module.exports = mergeTrees([appJs, appCss, publicFiles, testsJs, testFiles])
